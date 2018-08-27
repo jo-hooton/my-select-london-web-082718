d6eae55a6117992d4dd965cff979183b98becbb5
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-27 18:57:01 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  if block_given?
+    i = 0
+  else puts "This block should not run!"
+
+    new_array = []
+
+    while i < array.length
+      result = yield(array[i])
+      i = i + 1
+
+      new_array.push(result)
+    end
+    new_array
+  end
 end
